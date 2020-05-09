f8fcfb1009521546ddf052305a8f0a4ab7a5a883
James Martinez <airtorque1@gmail.com>
2020-05-09 07:07:19 -0400

    heroku fix

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const port = 3000;
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 
+const db = mongoose.connection;
 
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/demo-2';
+// Connect to Mongo
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+// open the connection to mongo
+db.on('open' , ()=>{});
 
 //middleware
 app.use(express.static('public'))
@@ -16,14 +27,7 @@ app.engine('jsx',require('express-react-views').createEngine());
 app.use(express.static('public')); //not used in this particular exercise,but almost always part of the middleware
 app.use(methodOverride('_method'));
 
-
-//Refactor this to be more like heroku project????
 //remember to set node.js engine version in package.json
-//mongoose Connection
-mongoose.connect('mongodb://localhost:27017/basiccrud',{useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.once('open',()=>{
-    console.log("connected to mongo");
-})
 
 //controller
 const tracksController = require('./controllers/tracks.js');
